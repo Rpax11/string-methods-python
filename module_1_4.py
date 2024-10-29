c19d262f216ad87343ea9c70e1c10edbf917e5e2
Rpax11 <Rapax11@gmail.com>
2024-10-29 22:58:03 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+from multiprocessing.spawn import old_main_modules
+
+my_string = input('введите произвольное колличество символов:')
+print('',my_string.upper())
+print('',my_string.lower())
+print('',my_string.replace(' ', ''))
+print('',my_string[0])
+print('',my_string[-1])
